7a8fd80f00ac342fb94d4c68359925af0faf5131
Welpike <pascal.lleonart@gmail.com>
2022-12-15 16:35:18 +0100

    add the "add grade" feature (to do : save the grade in the db)

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-from flask import Flask, render_template, url_for
+from flask import Flask, render_template, url_for, request, redirect
+
+from models import Grade
 
 app = Flask(__name__)
 
@@ -8,5 +10,28 @@ def index():
     return render_template('index.html')
 
 
+@app.route('/grade/new')
+def new_grade():
+
+    # get if the form is submitted or not
+    try:
+        form = request.args["form"]
+    except Exception:
+        form = False
+
+    # if form == submitted, create grade with args in url and then redirect on the home
+    if form:
+        value = request.args["value"]
+        factor = request.args["factor"]
+        subject = request.args["subject"]
+        description = request.args["description"]
+
+        new_grade = Grade(value=value, factor=factor, subject=subject, description=description)
+        return render_template('add_grade.html', grade=new_grade)
+        # return redirect('/')
+    else:  # if form != submitted show template
+        return render_template('add_grade.html', grade="")
+
+
 if __name__ == "__main__":
-    app.run()
+    app.run(debug=True)
---
+++ models.py
@@ -0,0 +1,8 @@
+
+class Grade:
+    def __init__(self, value, subject, description, factor):
+        self.value = value
+        self.subject = subject
+        self.description = description
+        self.factor = factor
+
